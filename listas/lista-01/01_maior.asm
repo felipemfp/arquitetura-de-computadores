b5e66466a16e082fe73ac0ce8a22b241d1b8bc96
Felipe Pontes <felipemfpontes@gmail.com>
2016-10-02 07:14:38 -0300

    Update listas/lista-01/01_maior.asm

--- listas/lista-01/01_maior.asm
+++ listas/lista-01/01_maior.asm
@@ -1,15 +1,17 @@
 main:
-  addi $1, $0, 20
+  addi $1, $0, 80
   addi $2, $0, 40
   slt $4, $1, $2
   beq $4, $0, show_a
   j show_b
 
-show_b:
-  add $3, $0, $2
-  nop 
-
 show_a:
   add $3, $0, $1
-  nop
+  j exit
 
+show_b:
+  add $3, $0, $2
+  j exit 
+
+exit:
+  nop
